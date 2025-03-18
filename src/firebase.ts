f1d3e508aefcdf65a542f835fa8cd0bebcb06ede
Raphael Sanchez <sidrozmc@gmail.com>
2025-03-18 15:22:02 +0100

    Ajout de la configuration Firebase

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJNivR46GFAikF20ymAm7V-TwGLrNp8VI",
+  authDomain: "moncourtier-35d12.firebaseapp.com",
+  projectId: "moncourtier-35d12",
+  storageBucket: "moncourtier-35d12.firebasestorage.app",
+  messagingSenderId: "52548623672",
+  appId: "1:52548623672:web:62851e3b38645c3368b5cd",
+  measurementId: "G-2Y0EW7FF2Y"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
